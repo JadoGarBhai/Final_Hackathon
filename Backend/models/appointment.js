da07d029731f50c8005a074abac55432c30bad63
JadoGarBhai <sikandernawaz35@gmail.com>
2023-12-13 17:32:32 +0500

    'update'

--- Backend/models/appointment.js
+++ Backend/models/appointment.js
@@ -1,21 +1,34 @@
-    const mongoose = require('mongoose');
-    const appointmentSchema = new mongoose.Schema({
-        _id: mongoose.Schema.ObjectId,
-        name: {
-            type: String,
-            required: true
-        },
-        phone: {
-            type: Number,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true  
-        }
+const mongoose = require("mongoose");
+const appointmentSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.ObjectId,
+    doctorName: {
+      type: String,
+      required: true,
     },
-        { collection: "appointment", versionKey: false });
+    specialization: {
+      type: String,
+      required: true,
+    },
+    timing: {
+      type: String,
+      required: true,
+    },
+    patientName: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { collection: "appointment", versionKey: false }
+);
 
-    const Appointment = mongoose.model('appointment', appointmentSchema);
-    module.exports = Appointment;
+const Appointment = mongoose.model("appointment", appointmentSchema);
+module.exports = Appointment;
